1b5f1e6796beec17fdbb032ba3afc5db945585df
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-11-17 01:15:49 +0530

    Added conditional rendering in nav bar

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Link, withRouter} from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth/helper';
+
 
 const currentTab = (history, path) => {
     if(history.location.pathname === path){
@@ -33,21 +35,34 @@ const Menu = ({history}) => (
                     A. Dashboard
                 </Link>
             </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, '/signup')} className="nav-link"  to='/signup'>
-                    Signup
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, '/signin')} className="nav-link"  to='/signin'>
-                    Signin
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, '/signout')} className="nav-link"  to='/signout'>
-                    Signout
-                </Link>
-            </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                <li className="nav-item">
+                    <Link style={currentTab(history, '/signup')} className="nav-link"  to='/signup'>
+                        Signup
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link style={currentTab(history, '/signin')} className="nav-link"  to='/signin'>
+                        Signin
+                    </Link>
+                </li>
+            </Fragment>
+            )}
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                     className="nav-link text-warning" 
+                     onClick={() => {
+                         signout(()=>{
+                             history.push("/")
+                         })
+                     }} 
+                     style={{cursor:"pointer"}}>
+                        Signout
+                    </span>
+                </li>
+            )}
         </ul>
         
     </div>
