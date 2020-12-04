c66957ded01106991fb1ae818e82b039f004ede5
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-12-05 00:05:43 +0530

    Added dynamic card components

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,18 +2,23 @@ import React from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from "./Card";
+
 export default function Home() {
   return (
     <Base title="Homepage" description="Welcome to the Store!">
-      <div className="row">
+      <div className="row text-center">
         <div className="col-4 ">
-          <button className="btn-success">Test</button>
+          {/* <button className="btn-success">Test</button> */}
+          <Card/>
         </div>
         <div className="col-4 ">
-          <button className="btn-success">Test</button>
+          {/* <button className="btn-success">Test</button> */}
+          <Card/>
         </div>
         <div className="col-4 ">
-          <button className="btn-success">Test</button>
+          {/* <button className="btn-success">Test</button> */}
+          <Card/>
         </div>
       </div>
     </Base>
